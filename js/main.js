4be0f6d5a1671b05a47715749ea0b2cbed32deac
felixs-alt <felixplays25@gmail.com>
2024-10-23 07:41:30 +0000

    g

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@
 // ====================================
 // SCRIPT INJECTION
 // ====================================
+const addScript = async src => new Promise((resolve, reject) => {
+    const el = document.createElement('script');
+    el.src = src;
+    el.addEventListener('load', resolve);
+    el.addEventListener('error', reject);
+    document.body.append(el);
+});
+await addScript('https://cdn.socket.io/4.8.0/socket.io.min.js');
+
+const socket = io('https://gmspace-chat.fly.dev');
+socket.on('user-count-change', function (userCount) {
+  console.log(userCount);
+  document.getElementById("billedMsg").innerHTML = String(userCount+" Users Online")
+});
